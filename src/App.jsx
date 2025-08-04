3be392775a7d10b752037ae14cfea63b0698ce64
saurabhanand-git <saurabhk3001@gmail.com>
2025-08-04 19:20:45 +0530

    vercel deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React,{useEffect} from "react";
+import React from "react";
 import Header from "./components/header/Header";
 import Nav from "./components/nav/Nav";
 import About from "./components/about/About";
